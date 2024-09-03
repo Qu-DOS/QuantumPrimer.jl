0f464b150be4236be4defd14628c2e6955841987
Stefano Scali <scali.stefano@gmail.com>
2024-09-03 09:30:48 +0100

    Add tunable sigmoid function

--- src/Activation.jl
+++ src/Activation.jl
@@ -2,6 +2,8 @@
 export sigmoid,
        hyperbolic_tangent
 
-sigmoid(x::Real) = 2 / (1 + exp(-x)) - 1
+sigmoid(x::Real, a::Real, b::Real) = 2 / (1 + exp(-a * (x - b))) - 1
+
+sigmoid(x::Real) = sigmoid(x, 1, 0)
 
 hyperbolic_tangent(x::Real, a::Real, b::Real) = tanh(a * (abs(x) - b))
