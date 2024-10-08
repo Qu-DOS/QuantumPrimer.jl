6a34fbe7d3eee94b30bc6ac96022f3f08ee0b6d8
Chukwudubem Umeano <chiddy1999@gmail.com>
2024-10-08 10:29:59 +0100

    Fixed QSP functions

--- src/QSP.jl
+++ src/QSP.jl
@@ -9,44 +9,44 @@ export W,
        QSVT_square
 
 """
-    W(a::Real) -> ChainBlock
+    W(a::Float64) -> ChainBlock
 
 Creates a quantum circuit block for the W gate.
 
 # Arguments
-- `a::Real`: The parameter for the W gate.
+- `a::Float64`: The parameter for the W gate.
 
 # Returns
 - `ChainBlock`: The quantum circuit block for the W gate.
 """
-W(a::Real) = chain(X, Ry(-2asin(a)))
+W(a::Float64) = chain(X, Ry(-2asin(a)))
 
 """
-    S(phi::Real) -> Rz
+    S(phi::Float64) -> Rz
 
 Creates a quantum gate for the S gate.
 
 # Arguments
-- `phi::Real`: The parameter for the S gate.
+- `phi::Float64`: The parameter for the S gate.
 
 # Returns
 - `Rz`: The quantum gate for the S gate.
 """
-S(phi::Real) = Rz(-2(phi)) 
+S(phi::Float64) = Rz(-2(phi)) 
 
 """
-    Usp(phis::Vector{Real}, a::Real) -> ChainBlock
+    Usp(phis::Vector{Float64}, a::Float64) -> ChainBlock
 
 Creates a quantum circuit for the Usp unitary.
 
 # Arguments
-- `phis::Vector{Real}`: The vector of phase parameters.
-- `a::Real`: The parameter for the W gate.
+- `phis::Vector{Float64}`: The vector of phase parameters.
+- `a::Float64`: The parameter for the W gate.
 
 # Returns
 - `ChainBlock`: The quantum circuit for the Usp unitary.
 """
-function Usp(phis::Vector{Real}, a::Real)
+function Usp(phis::Vector{Float64}, a::Float64)
     d = length(phis)
     circ = chain(1)
     for k = 1:d-1
@@ -57,47 +57,47 @@ function Usp(phis::Vector{Real}, a::Real)
 end
 
 """
-    eval_Usp(x::Real, phis::Vector{Real}) -> Real
+    eval_Usp(x::Float64, phis::Vector{Float64}) -> Float64
 
 Evaluates the Usp unitary for a given input and phase parameters.
 
 # Arguments
-- `x::Real`: The input parameter.
-- `phis::Vector{Real}`: The vector of phase parameters.
+- `x::Float64`: The input parameter.
+- `phis::Vector{Float64}`: The vector of phase parameters.
 
 # Returns
-- `Real`: The evaluated value of the Usp unitary.
+- `Float64`: The evaluated value of the Usp unitary.
 """
-eval_Usp(x::Real, phis::Vector{Real}) = real(expect(Usp(phis, x), zero_state(1))) # Transform P(a)=<0|Usp|0> (or <+|Usp|+> if basis is changed) 
+eval_Usp(x::Float64, phis::Vector{Float64}) = real(sandwich(zero_state(1), Usp(phis, x), zero_state(1))) # Transform P(a)=<0|Usp|0> (or <+|Usp|+> if basis is changed) 
 
 """
-    loss(target::Function, xs::Vector{Real}, phis::Vector{Real}) -> Real
+    loss(target::Function, xs::Vector{Float64}, phis::Vector{Float64}) -> Float64
 
 Computes the loss function for a given target function, inputs, and phase parameters.
 
 # Arguments
 - `target::Function`: The target function.
-- `xs::Vector{Real}`: The vector of input parameters.
-- `phis::Vector{Real}`: The vector of phase parameters.
+- `xs::Vector{Float64}`: The vector of input parameters.
+- `phis::Vector{Float64}`: The vector of phase parameters.
 
 # Returns
-- `Real`: The computed loss.
+- `Float64`: The computed loss.
 """
-loss(target::Function, xs::Vector{Real}, phis::Vector{Real}) = sum(map(i -> (eval_Usp(xs[i], phis) - target(xs[i]))^2, 1:length(xs)))
+loss(target::Function, xs::Vector{Float64}, phis::Vector{Float64}) = sum(map(i -> (eval_Usp(xs[i], phis) - target(xs[i]))^2, 1:length(xs)))
 
 """
-    pcp(n::Int, phi::Real) -> ChainBlock
+    pcp(n::Int, phi::Float64) -> ChainBlock
 
 Creates a projected controlled phase gate.
 
 # Arguments
 - `n::Int`: The number of qubits.
-- `phi::Real`: The phase parameter.
+- `phi::Float64`: The phase parameter.
 
 # Returns
 - `ChainBlock`: The quantum circuit block for the projected controlled phase gate.
 """
-pcp(n::Int, phi::Real) = chain(n+1, put(n+1 => Rz(ComplexF64(-2phi)))) # projected controlled phase gate
+pcp(n::Int, phi::Float64) = chain(n+1, put(n+1 => Rz(ComplexF64(-2phi)))) # projected controlled phase gate
 
 """
     block_encode2(n::Int, A::AbstractMatrix) -> ChainBlock
@@ -114,20 +114,20 @@ Creates a block encoding for a given matrix.
 block_encode2(n::Int, A::AbstractMatrix) = chain(n+1, put(n+1 => X), control(n+1, 1:n => matblock(ComplexF64.(A))), put(n+1 => X)) # example block encoding method
 
 """
-    QSVT_square(n::Int, d::Int, phis::Vector{Real}, A::AbstractMatrix) -> ChainBlock
+    QSVT_square(n::Int, d::Int, phis::Vector{Float64}, A::AbstractMatrix) -> ChainBlock
 
 Creates a quantum singular value transformation (QSVT) circuit for a given matrix.
 
 # Arguments
 - `n::Int`: The number of qubits.
 - `d::Int`: The degree of the polynomial.
-- `phis::Vector{Real}`: The vector of phase parameters.
+- `phis::Vector{Float64}`: The vector of phase parameters.
 - `A::AbstractMatrix`: The matrix to be transformed.
 
 # Returns
 - `ChainBlock`: The quantum circuit for the QSVT.
 """
-function QSVT_square(n::Int, d::Int, phis::Vector{Real}, A::AbstractMatrix)
+function QSVT_square(n::Int, d::Int, phis::Vector{Float64}, A::AbstractMatrix)
     circ = chain(n+1)
     if d % 2 == 0 # even d
         for i = div(d, 2):-1:1
